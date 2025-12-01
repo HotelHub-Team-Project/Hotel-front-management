dfe1df61be684e9d73d4c81ea845d569d25d2c69
wkdwlgns12 <jjg530@kbu.ac.kr>
2025-12-01 21:09:56 +0900

    init

--- src/api/adminCouponApi.js
+++ src/api/adminCouponApi.js
@@ -1,11 +1,14 @@
-import axiosClient from "./axiosClient";
+import { mockCouponApi } from "./mockApi";
+
+// 실제 API 연동 시 axiosClient 사용, 지금은 Mock 사용
+const USE_MOCK = true;
 
 export const adminCouponApi = {
-  getCoupons: () => axiosClient.get("/coupons"),
-  getCouponById: (id) => axiosClient.get(`/coupons/${id}`),
-  createCoupon: (data) => axiosClient.post("/coupons", data),
-  updateCoupon: (id, data) => axiosClient.put(`/coupons/${id}`, data),
-  deleteCoupon: (id) => axiosClient.delete(`/coupons/${id}`),
+  getCoupons: () => mockCouponApi.getCoupons(),
+  getCouponById: (id) => mockCouponApi.getCouponById(id),
+  createCoupon: (data) => mockCouponApi.createCoupon(data),
+  updateCoupon: (id, data) => mockCouponApi.updateCoupon(id, data),
+  deleteCoupon: (id) => mockCouponApi.deleteCoupon(id),
 };
 
 export default adminCouponApi;
